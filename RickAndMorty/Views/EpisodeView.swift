968466560a7d299563b495db081393b872476de9
Salman <salzahir18@gmail.com>
2025-02-02 19:53:44 -0600

    Improved episode view deails

--- RickAndMorty/Views/EpisodeView.swift
+++ RickAndMorty/Views/EpisodeView.swift
@@ -13,8 +13,33 @@ struct EpisodeView: View {
     let episodeURL: String
     
     var body: some View {
-        VStack {
-            Text(episode?.name ?? "None")
+        
+        ScrollView {
+            if let episode = episode {
+                
+                Text(episode.name)
+                    .font(.title2)
+                    .padding(.top)
+                
+                Text("Air Date: \(episode.air_date)")
+                    .padding(.top, 5)
+                
+                Text("Episode: \(episode.episode)")
+                    .padding(.top, 5)
+                
+                Text("Characters: \(episode.characters.joined(separator: ", "))")
+                    .padding(.top, 5)
+                
+                Text("URL: \(episode.url)")
+                    .padding(.top, 5)
+                
+                Text("Created: \(episode.created)")
+                    .padding(.top, 5)
+            } else {
+                Text("No episode data available")
+                    .italic()
+                    .padding(.top)
+            }
         }
         .task {
             do {
